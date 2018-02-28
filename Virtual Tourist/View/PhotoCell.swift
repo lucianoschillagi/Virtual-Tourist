a3bba727d512374061e509ab87ed3b5758971b92
Luciano Schillagi <luko.web@gmail.com>
2018-02-28 14:50:59 -0300

    refactor: Remove 'Autolayout' file

--- Virtual Tourist/View/PhotoCell.swift
+++ Virtual Tourist/View/PhotoCell.swift
@@ -18,38 +18,4 @@ class PhotoCell: UICollectionViewCell {
 	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	override func prepareForReuse() {
-//		photoImageView.image = nil // BORRAR? [sacado de RW video]
-//	}
-	
-	// computed property
-	
-	// INCOMPLETO, LUEGO VER SI IMPLEMENTO
-//	var flickrImage: FlickrImage? {
-//
-//		didSet {
-//			if let nationalPark = park {
-//				parkImageView.image = UIImage(named: nationalPark.photo)
-//			}
-//		}
-//	}
-	
-//	var park: Park? {
-//
-//		didSet {
-//			if let nationalPark = park {
-//				parkImageView.image = UIImage(named: nationalPark.photo)
-//			}
-//		}
-//	}
-	
 } // end class
